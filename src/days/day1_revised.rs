f819afd48307d33d361d4e0f43416ffa87d5c7e7
Omnikar <Omnikar5@gmail.com>
2021-12-01 12:16:38 -0500

    Day 1 revised solution

---
+++ src/days/day1_revised.rs
@@ -0,0 +1,27 @@
+fn parse(input: String) -> Result<Vec<u32>, std::num::ParseIntError> {
+    input
+        .trim()
+        .split('\n')
+        .map(|s| s.parse::<u32>())
+        .collect::<Result<Vec<_>, _>>()
+}
+
+fn part1(input: String) -> crate::Result {
+    let nums = parse(input)?;
+    let count = nums.windows(2).filter(|w| w[1] > w[0]).count();
+
+    println!("{}", count);
+
+    Ok(())
+}
+
+fn part2(input: String) -> crate::Result {
+    let nums = parse(input)?;
+    let count = nums.windows(4).filter(|w| w[3] > w[0]).count();
+
+    println!("{}", count);
+
+    Ok(())
+}
+
+crate::parts!(part1 part2);
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -1,3 +1,5 @@
 mod day1;
 
-crate::days!(day1);
+mod day1_revised;
+
+crate::days!(day1 day1_revised);
