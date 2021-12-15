f100a9e739516c243c0f6479f5acbbcaad878fd7
Omnikar <omnikar5@gmail.com>
2021-12-14 23:06:03 -0500

    Complete Day 14

--- src/days/day14.rs
+++ src/days/day14.rs
@@ -1,46 +1,89 @@
 use std::collections::HashMap as Map;
 
-fn parse(input: &str) -> (Vec<u8>, Map<(u8, u8), u8>) {
+fn parse(input: &str) -> (Vec<usize>, Vec<Vec<usize>>) {
+    let mut letters = Vec::<u8>::new();
     let (template, rest) = input.trim().split_once("\n\n").unwrap();
-    let template = template.into();
-    let map = rest
+    let mut get = |l| {
+        letters
+            .iter()
+            .enumerate()
+            .find_map(|(i, n)| (*n == l as u8).then(|| i))
+            .unwrap_or_else(|| {
+                letters.push(l);
+                letters.len() - 1
+            })
+    };
+    let map_contents = rest
         .trim()
         .lines()
         .map(|l| {
             let (pair, letter) = l.split_once(" -> ").unwrap();
             let mut pair = pair.chars();
-            let a = pair.next().unwrap() as u8;
-            let b = pair.next().unwrap() as u8;
-            let letter = letter.chars().next().unwrap() as u8;
+            let a = get(pair.next().unwrap() as u8);
+            let b = get(pair.next().unwrap() as u8);
+            let letter = get(letter.chars().next().unwrap() as u8);
             ((a, b), letter)
         })
-        .collect();
+        .collect::<Vec<_>>();
+    let template = template.bytes().map(|b| get(b)).collect();
+    let mut map = vec![vec![0; letters.len()]; letters.len()];
+    for ((a, b), n) in map_contents {
+        map[a][b] = n;
+    }
     (template, map)
 }
 
-fn simulate(input: &str, count: u32) -> u32 {
-    let (mut template, map) = parse(input);
+fn grow(
+    known: &mut Map<((usize, usize), u64), Vec<u64>>,
+    map: &[Vec<usize>],
+    (a, b): (usize, usize),
+    count: u64,
+) -> Vec<u64> {
+    if let Some(freqs) = known.get(&((a, b), count)) {
+        return freqs.to_owned();
+    }
+    let mut freqs = vec![0; map.len()];
 
-    for i in 0..count {
-        println!("{}", i);
-        let mut new = Vec::new();
-        template.windows(2).rev().zip(1..).for_each(|(pair, i)| {
-            let pair = (pair[0], pair[1]);
-            if let Some(l) = map.get(&pair) {
-                new.push((*l, template.len() - i));
-            }
-        });
-        new.into_iter().for_each(|(n, i)| template.insert(i, n));
-        println!("len: {}", template.len());
+    if count == 1 {
+        freqs[a] += 1;
+        let n = map[a][b];
+        freqs[n] += 1;
+    } else {
+        let n = map[a][b];
+        let new_freqs_0 = grow(known, map, (a, n), count - 1);
+        let new_freqs_1 = grow(known, map, (n, b), count - 1);
+        [new_freqs_0, new_freqs_1]
+            .into_iter()
+            .for_each(|new_freqs| {
+                freqs
+                    .iter_mut()
+                    .zip(new_freqs.into_iter())
+                    .for_each(|(n, count)| *n += count)
+            });
     }
 
-    let mut freqs = Map::new();
-    template
-        .into_iter()
-        .for_each(|n| *freqs.entry(n).or_insert(0) += 1);
-    let m_common = freqs.iter().max_by_key(|(_, count)| *count).unwrap();
-    let l_common = freqs.iter().min_by_key(|(_, count)| *count).unwrap();
-    m_common.1 - l_common.1
+    known.entry(((a, b), count)).or_insert(freqs).to_owned()
+}
+
+fn simulate(input: &str, count: u64) -> u64 {
+    let (template, map) = parse(input);
+
+    let mut freqs = vec![0; map.len()];
+    freqs[*template.last().unwrap()] = 1;
+
+    for pair in template.windows(2) {
+        let pair = (pair[0], pair[1]);
+        let mut known = Map::new();
+        let new_freqs = grow(&mut known, &map, pair, count);
+        freqs
+            .iter_mut()
+            .zip(new_freqs.into_iter())
+            .for_each(|(n, count)| *n += count);
+    }
+
+    let m_common = freqs.iter().copied().max().unwrap();
+    let l_common = freqs.iter().copied().min().unwrap();
+    m_common - l_common
 }
 
 fn part1(input: &str) {
@@ -48,10 +91,9 @@ fn part1(input: &str) {
     println!("{}", ans);
 }
 
-// Current algorithm is too inefficient for part 2
-// fn part2(input: &str) {
-//     let ans = simulate(input, 40);
-//     println!("{}", ans);
-// }
+fn part2(input: &str) {
+    let ans = simulate(input, 40);
+    println!("{}", ans);
+}
 
-crate::parts!(part1);
+crate::parts!(part1 part2);
